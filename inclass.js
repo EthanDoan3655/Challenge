125d069e0d69f9ae17ff3f811d86674a4b9041ab
Ethan Doan <ethandoan10@gmail.com>
2022-04-29 13:53:23 -0400

    first commit

---
+++ inclass.js
@@ -0,0 +1,18 @@
+/*1 Convert the following function into an arrow function named "arrowMyFunction".
+const myFunction = function (num1, num2) {
+    return num1 + num2;
+}; */
+const num1=9;
+const num2=9;
+
+const arrowMyFunction = (num1,num2)=>{return num1 + num2};
+
+console.log(arrowMyFunction(num1,num2));
+
+/*2 Convert the following function into a one-lined arrow function without a return statement (the return is implicit) . Name the function arrowAdd100
+const add100 = function (money) {
+    return money + 100;
+}; */
+
+const arrowAdd100 = ("money"+ 100) =>{ }
+
