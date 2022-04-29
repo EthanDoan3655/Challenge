f54a08bfacbef739c2c18b50aa10c8ae7d57ef5a
Ethan Doan <ethandoan10@gmail.com>
2022-04-29 14:10:05 -0400

    please3

--- inclass.js
+++ inclass.js
@@ -16,6 +16,12 @@ const val1= {x}
 const val2= 100
 const arrowAdd100 = (val, val2) =>val + val2
 
+/*3 Create an arrow function that takes TWO arguments, a string and a single letter.  The function will count the number of times the specified letter occurs in the string. **Not case sensitive
+Examples:
+countLetters('Hello my name is John', 'i') returns 1
+countLetters('Hello my name is John', 'h') returns 2
+countLetters('We love this city a lot, sometimes', 'e') returns 4 */
+
 
 
 
